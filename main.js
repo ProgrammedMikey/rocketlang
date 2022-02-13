4af9e61950f604a9c2a82d4a46178633a82c5e5f
Michael DaSilva <programmedmikey@gmail.com>
2022-02-13 01:39:41 +0000

    completed globe and styling

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 import * as THREE from 'three'
+import gsap from 'gsap'
 import vertexShader from './shaders/vertex.glsl'
 import fragmentShader from './shaders/fragment.glsl'
 import atmosphereVertexShader from './shaders/atmosphereVertex.glsl'
@@ -6,14 +7,24 @@ import atmosphereFragmentShader from './shaders/atmosphereFragment.glsl'
 
 
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+const canvasContainer = document.querySelector('#canvasContainer')
+
+const camera = new THREE.PerspectiveCamera( 
+  75, 
+  canvasContainer.offsetWidth / canvasContainer.offsetHeight, 
+  0.1, 
+  1000 
+);
 
 const renderer = new THREE.WebGLRenderer({
-  antialias: true
+  antialias: true,
+  canvas: document.querySelector('canvas')
 });
-renderer.setSize( window.innerWidth, window.innerHeight );
+
+renderer.setSize( canvasContainer.offsetWidth, canvasContainer.offsetHeight );
 renderer.setPixelRatio(window.devicePixelRatio)
-document.body.appendChild( renderer.domElement );
+// document.body.appendChild( renderer.domElement );
 
 const sphere = new THREE.Mesh(
   new THREE.SphereGeometry(5, 50, 50), 
@@ -23,13 +34,11 @@ const sphere = new THREE.Mesh(
     uniforms: {
       globeTexture: {
         value: new THREE.TextureLoader().load(
-          './img/eTexture2.jpg')
+          './img/earthTexture2.jpg')
       }
     }
   })
 )
-
-scene.add(sphere)
 
 const atmosphere = new THREE.Mesh(
   new THREE.SphereGeometry(5, 50, 50), 
@@ -41,15 +50,53 @@ const atmosphere = new THREE.Mesh(
   })
 )
 atmosphere.scale.set(1.1, 1.1, 1.1)
+
 scene.add(atmosphere)
+
+const group = new THREE.Group()
+group.add(sphere)
+scene.add(group)
+
+const starGeometry = new THREE.BufferGeometry()
+const starmaterial = new THREE.PointsMaterial({ 
+    color: 0xffffff
+})
+
+const starVertices = []
+for (let i=0; i < 10000; i++) {
+  const x = (Math.random() - 0.5) * 2000
+  const y = (Math.random() - 0.5) * 2000
+  const z= -Math.random() * 2000
+  starVertices.push(x, y, z)
+}
+
+starGeometry.setAttribute('position', 
+  new THREE.Float32BufferAttribute(starVertices, 3))
+
+const stars = new THREE.Points(starGeometry, starmaterial)
+scene.add(stars)
 
 camera.position.z = 15
 
+const mouse = {
+  x: undefined,
+  y: undefined
+}
 
 function animate() {
   requestAnimationFrame(animate)
   renderer.render(scene, camera)
-  sphere.rotation.y += 0.001
+  sphere.rotation.y += 0.003
+  // group.rotation.y = mouse.x * 0.5
+  gsap.to(group.rotation, {
+    x: -mouse.y * 0.3,
+    y: mouse.x * 0.5,
+    duration: 2
+  })
 }
-
 animate()
+
+addEventListener('mousemove', () => {
+  mouse.x = (event.clientX / innerWidth) * 2 - 1
+  mouse.y = -(event.clientX / innerHeight) * 2 + 1
+})
